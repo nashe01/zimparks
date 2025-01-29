92756b98d5a7630fbdd121a9625bdfef0a6cbe70
nashe01 <mupindu021@gmail.com>
2025-01-29 17:28:06 +0200

    Initial commit

---
+++ supervisor/footer.php
@@ -0,0 +1,5 @@
+<div class="pull-right">
+		<footer>
+           <p>ZIMBABWE PARKS AND WILDLIFE MANAGEMENT AUTHORITY</p>
+        <footer>
+</div>
---
+++ title_index.php
@@ -0,0 +1,33 @@
+				
+<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span10">
+						<!--<img class="index_logo" src="admin/images/logo.jpg"> -->
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+								
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						
+								<div class="motto">
+										<p>ZIMPARKS FILE MANAGER </p>
+												<!--<p>2023</p> -->
+												
+								</div>		
+						</div>		
+				</div>
